8f05cd046c7fbf525f1a279f19f04a7f8edfe803
eljamaaouy ayman <eljamaaouyayman@eljamaaouys-MacBook-Air.local>
2026-01-26 17:15:07 -0800

    first exercice is done

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <limits>
+#include <iomanip>
+
+class ScalarConverter{
+    private:
+        ScalarConverter();
+        ScalarConverter(const ScalarConverter& other);
+        ScalarConverter& operator=(const ScalarConverter& other);
+        ~ScalarConverter();
+    public:
+        static void convert(std::string str);
+};
